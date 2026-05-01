17f3cc67d23278d92e3c066ae5df3cabbbbb0755
Eric Atekpani <eric.atekpani@uni-muenster.de>
2026-05-01 13:22:03 +0200

    me Controller

--- src/main/java/com/web/messanger/controller/UserController.java
+++ src/main/java/com/web/messanger/controller/UserController.java
@@ -1,11 +1,19 @@
 package com.web.messanger.controller;
 
 import com.web.messanger.model.User;
+import com.web.messanger.model.UserPrincipal;
 import com.web.messanger.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RequestMapping("/user")
 @RestController
@@ -16,6 +24,21 @@ public class UserController {
   public UserController(UserService service) {
     this.service = service;
   }
+
+  @GetMapping("/me")
+  public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal UserPrincipal user) {
+
+    if (user == null) {
+        return ResponseEntity.status(401).body("Not logged in");
+    }
+
+    return ResponseEntity.ok(
+      Map.of(
+        "username", user.getUsername()
+      )
+    );
+  }
+  
 
   @PostMapping("/register")
   public void register(@RequestBody User user) {
