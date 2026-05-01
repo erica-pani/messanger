e0dc27e45ea3c5dad776293a530bc519e52299f3
Eric Atekpani <eric.atekpani@uni-muenster.de>
2026-05-01 13:25:59 +0200

    spotlessApply

--- src/main/java/com/web/messanger/controller/UserController.java
+++ src/main/java/com/web/messanger/controller/UserController.java
@@ -3,17 +3,14 @@ package com.web.messanger.controller;
 import com.web.messanger.model.User;
 import com.web.messanger.model.UserPrincipal;
 import com.web.messanger.service.UserService;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RequestMapping("/user")
 @RestController
@@ -29,16 +26,11 @@ public class UserController {
   public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal UserPrincipal user) {
 
     if (user == null) {
-        return ResponseEntity.status(401).body("Not logged in");
+      return ResponseEntity.status(401).body("Not logged in");
     }
 
-    return ResponseEntity.ok(
-      Map.of(
-        "username", user.getUsername()
-      )
-    );
+    return ResponseEntity.ok(Map.of("username", user.getUsername()));
   }
-  
 
   @PostMapping("/register")
   public void register(@RequestBody User user) {
