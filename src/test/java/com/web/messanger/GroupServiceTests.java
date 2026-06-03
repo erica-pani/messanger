56a3d1c978687c703be5da24e5a5e7cf67032bcd
Eric Atekpani <eric.atekpani@uni-muenster.de>
2026-06-03 14:57:06 +0200

    weitere Test + aktivierter configuration cache

--- src/test/java/com/web/messanger/GroupServiceTests.java
+++ src/test/java/com/web/messanger/GroupServiceTests.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.web.messanger.model.ChatMessage;
 import com.web.messanger.model.Group;
 import com.web.messanger.model.GroupDTO;
 import com.web.messanger.model.User;
@@ -15,6 +16,8 @@ import com.web.messanger.repos.GroupRepository;
 import com.web.messanger.repos.UserRepository;
 import com.web.messanger.service.GroupService;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.junit.jupiter.api.AfterEach;
@@ -26,6 +29,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @ExtendWith(MockitoExtension.class)
 public class GroupServiceTests {
@@ -38,28 +42,89 @@ public class GroupServiceTests {
 
   GroupDTO groupDTO;
   Group group;
-  String validName = "gruppe";
-  Long validId = 17L;
+  User user;
+  String validGroupName = "gruppe";
+  String username = "username";
+  Long validGroupId = 17L;
+  Long validUserId = 35628L;
 
   @BeforeEach
   public void setUpTestCase() {
     groupDTO = new GroupDTO();
     group = new Group();
-    group.setName(validName);
-    group.setId(validId);
+    group.setName(validGroupName);
+    group.setId(validGroupId);
+    user = new User();
+    user.setUsername(username);
+    user.setId(validUserId);
   }
 
   @AfterEach
   public void tearDowntestCase() {
     groupDTO = null;
     group = null;
+    user = null;
   }
 
   @Test
-  public void getRelevantGroupsTest() {}
+  public void getRelevantGroupsTest() {
+    user.setGroups(Set.of(group));
+
+    when(userRepository.findByUsername(username)).thenReturn(user);
+
+    Collection<Group> userGroups = target.getRelevantGroups(username);
+
+    assertEquals(1, userGroups.size());
+    assertTrue(userGroups.contains(group));
+  }
 
   @Test
-  public void loadGroupChatTest() {}
+  public void getRelevantGroups_whenUserNotFound() {
+    user.setGroups(Set.of(group));
+
+    when(userRepository.findByUsername(username)).thenReturn(null);
+
+    assertThrows(
+        UsernameNotFoundException.class,
+        () -> {
+          target.getRelevantGroups(username);
+        });
+  }
+
+  @Test
+  public void loadGroupChatTest() {
+
+    ChatMessage message1 = new ChatMessage();
+    ChatMessage message2 = new ChatMessage();
+
+    List<ChatMessage> messages = List.of(message1, message2);
+    group.setMessages(messages);
+
+    when(groupRepository.findByName(validGroupName)).thenReturn(group);
+
+    List<ChatMessage> result = target.loadGroupChat(validGroupName);
+
+    assertEquals(2, result.size());
+    assertTrue(result.contains(message1));
+    assertTrue(result.contains(message2));
+  }
+
+  @Test
+  public void loadGroupChat_whenGroupNotFound() {
+    ChatMessage message1 = new ChatMessage();
+    ChatMessage message2 = new ChatMessage();
+
+    List<ChatMessage> messages = List.of(message1, message2);
+    group.setMessages(messages);
+
+    when(groupRepository.findByName(validGroupName)).thenReturn(null);
+
+    assertThrows(
+        EntityNotFoundException.class,
+        () -> {
+          target.loadGroupChat(validGroupName);
+        });
+  }
 
   @Test
   public void createNewGroupTest() {
@@ -101,9 +166,9 @@ public class GroupServiceTests {
   @Test
   public void deleteGroupTest() {
 
-    when(groupRepository.findById(validId)).thenReturn(Optional.of(group));
+    when(groupRepository.findById(validGroupId)).thenReturn(Optional.of(group));
 
-    Group result = target.deleteGroup(validId, validName);
+    Group result = target.deleteGroup(validGroupId, validGroupName);
 
     assertEquals(group, result);
     verify(groupRepository).delete(result);
@@ -112,12 +177,12 @@ public class GroupServiceTests {
   @Test
   public void deleteGroup_whenNameDoesNotMatch() {
 
-    when(groupRepository.findById(validId)).thenReturn(Optional.of(group));
+    when(groupRepository.findById(validGroupId)).thenReturn(Optional.of(group));
 
     assertThrows(
         EntityNotFoundException.class,
         () -> {
-          target.deleteGroup(validId, "GruppeDieEsNichtGibt");
+          target.deleteGroup(validGroupId, "GruppeDieEsNichtGibt");
         });
 
     verify(groupRepository, never()).delete(any());
@@ -126,12 +191,12 @@ public class GroupServiceTests {
   @Test
   public void deleteGroup_whenNotFound() {
 
-    when(groupRepository.findById(validId)).thenReturn(Optional.empty());
+    when(groupRepository.findById(validGroupId)).thenReturn(Optional.empty());
 
     assertThrows(
         EntityNotFoundException.class,
         () -> {
-          target.deleteGroup(validId, validName);
+          target.deleteGroup(validGroupId, validGroupName);
         });
 
     verify(groupRepository, never()).delete(any());
